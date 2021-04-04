51efb0b9d06418de8572cd3bdf0f39a6e42e8fda
Andrej Strogonov <strogonovandrej@hotmail.com>
2021-04-04 22:48:29 +0300

    Full Source Project

--- app/src/main/java/com/strogonov/rightuniversity/MainActivity.kt
+++ app/src/main/java/com/strogonov/rightuniversity/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.strogonov.rightuniversity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // get reference to all views
+        var et_email = findViewById(R.id.et_email) as EditText
+        var et_password = findViewById(R.id.et_password) as EditText
+        var btn_login = findViewById(R.id.btn_login) as Button
+        var btn_reg = findViewById(R.id.btn_signup) as Button
+        //переход ко второму активити
+
+        btn_reg.setOnClickListener{
+            val intent = Intent(this@MainActivity, SignInActivity::class.java)
+            startActivity(intent)
+        }
+        // set on-click listener
+        btn_login.setOnClickListener {
+            val user_name = et_email.text;
+            val password = et_password.text;
+            val intent = Intent(this@MainActivity, University::class.java)
+            startActivity(intent)
+
+            // your code to validate the user_name and password combination
+            // and verify the same
+
+        }
     }
 }
